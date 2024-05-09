18b153676060f9f7b6a0fb390e4d05a2ebffb377
Sdpuchatt <87397648+Sdpuchatt@users.noreply.github.com>
2024-05-08 21:06:59 -0300

    desde casa

---
+++ pruebas/dosRetornos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int suma(int a,int b);
+int main()
+// main() es donde empieza la ejecución
+{
+
+cout << "Hola Mundo! des c++" << endl; // Muestra Hola mundo!
+int a = 2,b = 4;
+int resultado;
+resultado = mayorMenor(a,b);
+cout << resultado;
+return 0;
+
+}
+int mayorMenor(int a, int b){
+
+    return a;
+
+ 
+}
---
+++ pruebas/fecha.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int dia, mes ,anio;
+cout << "ingresa una fecha" << endl; // Muestra Hola mundo!
+cin >> dia >> mes >> anio;
+cout << "aca imprime la fecha "<<dia << mes << anio;
+return 0;
+
+}
